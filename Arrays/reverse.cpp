18494f527aa6a1838e6823dd9a7f28399e5bfb23
RitekPillai <mugiwaradluffy102@gmail.com>
2024-09-17 21:26:33 +0530

    reverse of a array

---
+++ Arrays/reverse.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+void reverse(int array[],int size){
+    for(int i=size-1;i<=0;i--){
+        cout<<array[i]<<" ";
+    }
+}
+int main(){
+    int array[100];
+    int n;
+    cout<<"Enter how many element you want to enter i a array"<<endl;
+    cin>>n;
+
+    cout<<"Enter a  Array"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>array[i];
+    }
+    reverse(array,n);
+}
